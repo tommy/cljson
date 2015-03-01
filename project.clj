7ba56393bb6847f8c2ddf28af5218c11516d6af5
Tommy Rogers <tommysayshello@gmail.com>
2015-03-01 13:07:54 -0800

    Make deployable

--- project.clj
+++ project.clj
@@ -7,18 +7,25 @@
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.omcljs/om "0.8.8"]]
 
-  :plugins [[lein-cljsbuild "1.0.4"]]
+  :plugins [[lein-cljsbuild "1.0.4"]
+            [org.clojure/data.json "0.2.5"]
+            [lein-s3-static-deploy "0.1.0"]]
+
+  :aws {:access-key ~(-> "secret.edn" slurp read-string :access)
+        :secret-key ~(-> "secret.edn" slurp read-string :secret)
+        :s3-static-deploy {:bucket "cljson.com"
+                           :local-root "public"}}
 
   :source-paths ["src" "target/classes"]
 
-  :clean-targets ["out/cljson" "out/cljson.js"]
+  :clean-targets ["public/build/cljson" "public/build/cljson.js"]
 
   :cljsbuild {
     :builds [{:id "cljson"
               :source-paths ["src"]
               :compiler {
-                :output-to "out/cljson.js"
-                :output-dir "out"
-                :optimizations :none
+                :output-to "public/build/cljson.js"
+                :output-dir "public/build"
+                :optimizations :advanced
                 :cache-analysis true
-                :source-map true}}]})
+                }}]})
