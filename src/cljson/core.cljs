0d6ce9a8f0271c79566048a947060724a4619979
Tommy Rogers <tommysayshello@gmail.com>
2015-03-01 09:11:03 -0800

    Add app

--- src/cljson/core.cljs
+++ src/cljson/core.cljs
@@ -1,16 +1,96 @@
 (ns cljson.core
   (:require [om.core :as om]
             [om.dom :as dom]
-            [cljson.connect]))
+            [cljs.core.async :refer [put! chan <!]]
+            [cljs.reader :refer [read-string]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (enable-console-print!)
+(println "Loaded core.")
 
-(def app-state (atom {:text "Hello world!"}))
+;;;;;;;;;;;;;;;;;;;;
+;; parsing functions
+
+(defn json->
+  [s]
+  (try
+    (-> s
+        JSON.parse
+        (js->clj :keywordize-keys true))
+    (catch :default e nil)))
+
+(defn edn->
+  [s]
+  (try
+    (read-string s)
+    (catch :default e nil)))
+
+;;;;;;;;;;;;;;;;;;;;;
+;; printing functions
+
+(def ->json
+  (comp #(JSON.stringify % nil 2)
+        clj->js))
+
+(def ->edn pr-str)
+
+
+;;;;;;;;;;;;;
+;; components
+
+(defn text-change
+  [e owner to-chan]
+  (let [v (.. e -target -value)]
+    (om/set-state! owner :text v)
+    (put! to-chan v)))
+
+(defn input-component
+  [placeholder]
+  (fn [data owner]
+    (reify
+      om/IInitState
+      (init-state [_]
+        {:text ""})
+
+      om/IWillMount
+      (will-mount [_]
+        (go (loop []
+              (when-let [change (<! (:from data))]
+                (when-let [parsed ((:read-fn data) change)]
+                  (println placeholder " Got a change: " parsed)
+                  (om/set-state! owner :text ((:write-fn data) parsed)))
+                (recur)))))
+
+      om/IRenderState
+      (render-state [this {:keys [text]}]
+        (dom/textarea #js {:style {}
+                           :placeholder placeholder
+                           :value text
+                           :onChange #(text-change % owner (:to data))})))))
+
+(def app-component
+  (fn [data owner]
+    (reify
+      om/IInitState
+      (init-state [_]
+        {:json-changes (chan)
+         :edn-changes (chan)})
+
+      om/IRenderState
+      (render-state [this {:keys [json-changes
+                                  edn-changes]}]
+        (dom/div nil
+                 (om/build (input-component "EDN")
+                           {:from json-changes :to edn-changes
+                            :write-fn ->edn :read-fn json->})
+                 (om/build (input-component "JSON")
+                           {:from edn-changes :to json-changes
+                            :write-fn ->json :read-fn edn->}))))))
+
+
+(def app-state (atom {}))
 
 (om/root
-  (fn [app owner]
-    (reify om/IRender
-      (render [_]
-        (dom/h1 nil (:text app)))))
+  app-component
   app-state
   {:target (. js/document (getElementById "app"))})
